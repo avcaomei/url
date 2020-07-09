317b9d5bc936c4c946e9256a1f51bbd110c505d8
avcaomei <42317919+avcaomei@users.noreply.github.com>
2020-07-10 04:38:55 +0800

    Create mmh.js

---
+++ mmh.js
@@ -0,0 +1,23 @@
+
+var host = window.location.host.toLowerCase();
+var url = new Array();
+var url2 = "";
+
+
+url.push("999911.xyz");
+url.push("999922.xyz");
+url.push("999955.xyz");
+url.push("999966.xyz");
+url.push("999977.xyz");
+
+
+//url2 = "http://" + escape(url[Math.floor(Math.random() * url.length)]);
+//setTimeout('location.href=url2',5000);
+for (var i = 0; i < url.length; i++) {
+	if (host.indexOf(url[i]) >= 0) {
+		istiao = "0"
+	}
+}
+if (istiao == "1") {
+	top.location.href = url2
+}
